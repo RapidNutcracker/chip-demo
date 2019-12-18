1af43165c4c41a69a769b3fe452d6470d8856a8c
Patrick Curran <rapidnutcracker@gmail.com>
2019-12-17 23:23:18 -0500

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import {
+    MatChipsModule,
+    MatIconModule
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { ChipsComponent } from './components/chips/chips.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ChipsComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        RouterModule.forRoot([
+            { path: '', redirectTo: 'chips', pathMatch: 'full' },
+            { path: 'chips/:category', component: ChipsComponent }
+        ]),
+        MatChipsModule,
+        MatIconModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/chips/chips.component.ts
@@ -0,0 +1,99 @@
+import { Component, OnInit } from '@angular/core';
+import { MatChipInputEvent } from '@angular/material';
+import { ENTER, COMMA } from '@angular/cdk/keycodes';
+import { ActivatedRoute, Router } from '@angular/router';
+
+interface Thing {
+    name: string;
+    color: string;
+}
+
+@Component({
+    selector: 'app-chips',
+    templateUrl: './chips.component.html',
+    styleUrls: ['./chips.component.scss']
+})
+export class ChipsComponent implements OnInit {
+    readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+
+    visible = true;
+    selectable = true;
+    removable = true;
+    addOnBlur = true;
+    category: string;
+    otherCategory: string;
+    displayThings: Thing[];
+
+    filters: any[];
+    allFilters: string;
+
+    things = {
+        fruits: [
+            { name: 'Lemon', color: 'yellow' },
+            { name: 'Lime', color: 'green' },
+            { name: 'Apple', color: 'red' },
+        ],
+        vegetables: [
+            { name: 'Broccoli', color: 'green' },
+            { name: 'Celery', color: 'green' },
+            { name: 'Zucchini', color: 'green' },
+            { name: 'Sweet Potato', color: 'orange' },
+        ]
+    };
+
+    private order: string;
+
+    constructor(private route: ActivatedRoute, private router: Router) {
+        this.route.params.subscribe(params => {
+            this.category = params.category;
+            this.otherCategory = this.category === 'fruits' ? 'vegetables' : 'fruits';
+            this.displayThings = this.things[this.category];
+        });
+
+        this.route.queryParams.subscribe(queryParams => {
+            this.displayThings = this.things[this.category];
+            this.filters = Object.entries(queryParams).map(([key, value]) => `${key}=${value}`);
+            this.allFilters = this.filters.join('&');
+
+            this.order = queryParams.order;
+            if (this.order === 'asc') {
+                this.displayThings = this.displayThings.sort((a, b) => a.name.localeCompare(b.name));
+            } else if (this.order === 'desc') {
+                this.displayThings = this.displayThings.sort((a, b) => b.name.localeCompare(a.name));
+            }
+
+            if (queryParams.color) {
+                this.displayThings = this.displayThings.filter((thing: Thing) => thing.color === queryParams.color);
+            }
+        });
+    }
+
+    ngOnInit() { }
+
+    remove(thing: any): void {
+        const index = this.filters.indexOf(thing);
+
+        if (index >= 0) {
+            this.filters.splice(index, 1);
+            this.allFilters = this.filters.join('&');
+            this.updateFilters(this.allFilters);
+        }
+    }
+
+    updateFilters(newFilterString: string) {
+
+        const newParams: any = {};
+        const splitFilters = newFilterString.split('&').map(filter => filter.split('='));
+        splitFilters.forEach(param => { newParams[param[0]] = param[1]; });
+
+        this.router.navigate(['.'], { relativeTo: this.route, queryParams: newParams });
+    }
+
+    clearFilters() {
+        this.updateFilters('');
+    }
+
+    goToOther() {
+        this.router.navigate([`/chips/${this.otherCategory}`], { queryParams: this.route.snapshot.queryParams });
+    }
+}
